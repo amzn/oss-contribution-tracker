71eca5651472e676c231f80da115390359d9d4b8
Jacob Peddicord <peddicor@amazon.com>
2018-02-21 13:50:18 -0800

    Replace gulp with npm scripts

--- selenium/navigation.spec.ts
+++ selenium/navigation.spec.ts
@@ -11,7 +11,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-import { By, until } from 'selenium-webdriver';
+import { By } from 'selenium-webdriver';
 import build, { CustomDriver } from './driver';
 
 describe('page navigation', function () {
